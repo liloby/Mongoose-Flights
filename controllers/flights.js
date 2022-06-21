0cc79bcd650a96944f58f8284779cf3ed6c46090
Oby <obyli214@gmail.com>
2022-06-21 00:09:05 -0700

    added default time bonus

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,32 +3,30 @@ const Flight = require('../models/flight')
 module.exports = {
     new: newFlight,
     create,
-    index
+    index,
 }
 
 
 function newFlight(req, res) {
-const newFlight = new Flight();
-// Obtain the default date
-const dt = newFlight.departs;
-// Format the date for the value attribute of the input
-let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
-departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
-res.render('flights/new', { departsDate });
+    const newFlight = new Flight();
+    // Obtain the default date
+    const dt = newFlight.departs;
+    // Format the date for the value attribute of the input
+    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+    res.render('flights/new', { departsDate });
 }
 
 function create(req, res) {
     const flight = new Flight(req.body)
     flight.save(function(err) {
         if (err) return res.redirect('/flights/new')
-        console.log(flight)
         res.redirect('/flights/new')
     })
 }
 
 function index(req, res) {
     Flight.find({}, function(err, flights) {
-        console.log(flights)
         res.render('flights/index', {
             flights
         })
--- models/flight.js
+++ models/flight.js
@@ -1,26 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 // optional shortcut to the mongoose.Schema class
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: {
-        type: String,
-        enum: ['American', 'Southwest', 'United'],
-    },
-    airport: {
-        type: String,
-        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
-        default: 'DEN'
-    },
-    flightNo: {
-        type: Number,
-        required: true,
-        min: 10,
-        max: 9999
-    },
-    departs: { type: Date, default: () => Date.now() + 365*24*60*60*1000}
-})
+  airline: {
+    type: String,
+    enum: ["American", "Southwest", "United"],
+  },
+  airport: {
+    type: String,
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    default: "DEN",
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999,
+  },
+  departs: {
+    type: Date,
+    default: Date.now() + 365 * 24 * 60 * 60 * 1000,
+  },
+});
 
-
-
-module.exports = mongoose.model('Flight', flightSchema)
+module.exports = mongoose.model("Flight", flightSchema);
